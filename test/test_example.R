bb2e623b64457ac25369f5fa06c157f8656f17c0
Davide <davide.messina@ars.toscana.it>
2021-06-03 21:11:58 +0200

    Reduced dependencies

--- test/test_example.R
+++ test/test_example.R
@@ -2,13 +2,15 @@ load("test/initial_df.RData")
 load("test/df_spells.RData")
 load("test/df_overlap.RData")
 
+source("R/CreateSpells_v14.R")
+
 df_spells_calculated <- CreateSpells(
   dataset = initial_df,
   id = "person_id",
   start_date = "op_start_date",
   end_date = "op_end_date",
   category ="op_meaning",
-  replace_missing_end_date = ymd(20250101),
+  replace_missing_end_date = lubridate::ymd(20250101),
   gap_allowed = 1,
   overlap = T,
   dataset_overlap = "df_overlap_calculated"
