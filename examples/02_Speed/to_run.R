cf56b8331f33f984b4766593107f393813662ed9
Davide Messina <davide.messina@ars.toscana.it>
2021-06-06 17:07:21 +0200

    Changed example

--- examples/02_Speed/to_run.R
+++ examples/02_Speed/to_run.R
@@ -3,8 +3,8 @@ source("R/CreateSpells_v14.R")
 
 row_to_gen <- 100000
 mean_n_obs_each_id <- 10
-n_meanings = 3
-n_origins = 3
+n_meanings = 5
+n_origins = 5
 
 source("examples/02_Speed/test_speed.R")
 
