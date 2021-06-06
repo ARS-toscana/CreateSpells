921494884f3f2d6bfaee4d5c688e592dd362ed25
Davide Messina <davide.messina@ars.toscana.it>
2021-06-06 16:40:49 +0200

    Finished example 2

--- examples/02_Speed/test_speed.R
+++ examples/02_Speed/test_speed.R
@@ -1,7 +1,6 @@
 library(data.table)
 source("R/CreateSpells_v14.R")
 
-row_to_gen <- 15000000
 seq_id <- seq_len(row_to_gen / 10)
 set.seed(123)
 generate_ids <- sample(seq_id, row_to_gen, replace = T)
@@ -16,21 +15,15 @@ test_df <- data.table(person_id = generate_ids,
                       op_meaning = generated_meanings,
                       op_origin = generated_origins)
 
-rm(generate_ids, generated_dates, generated_integers, generated_meanings, generated_origins)
+rm(row_to_gen, seq_id, generate_ids, generated_dates, generated_integers, generated_meanings, generated_origins)
 
-test <-  CreateSpells(
+
+print(system.time(CreateSpells(
   dataset = test_df,
   id = "person_id" ,
   start_date = "op_start_date",
   end_date = "op_end_date",
   category ="op_meaning"
-)
+)))
 
-fasttime::fastPOSIXct("2010-01-01")
-
-as.Date(generated_dates, "%Y%m%d")
-
-bench::mark(
-  lubridate::ymd(generated_dates),
-  as.Date(generated_dates, generated_dates)
-)
+rm(test_df)
---
+++ examples/02_Speed/to_run.R
@@ -0,0 +1,6 @@
+library(data.table)
+source("R/CreateSpells_v14.R")
+
+row_to_gen <- 1000000
+
+source("examples/02_Speed/test_speed.R")
