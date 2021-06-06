0c6b03946c75d690145ebae10794c66a1f0e3031
Davide Messina <davide.messina@ars.toscana.it>
2021-06-06 16:57:21 +0200

    Better example 2

--- examples/02_Speed/test_speed.R
+++ examples/02_Speed/test_speed.R
@@ -1,13 +1,13 @@
 library(data.table)
 source("R/CreateSpells_v14.R")
 
-seq_id <- seq_len(row_to_gen / 10)
+seq_id <- seq_len(as.integer(row_to_gen / mean_n_obs_each_id))
 set.seed(123)
 generate_ids <- sample(seq_id, row_to_gen, replace = T)
 generated_dates <- sample(seq.Date(Sys.Date(), Sys.Date() + 1000, by = "day"), row_to_gen, replace = T)
 generated_integers <- sample(seq_len(2000), row_to_gen, replace = T)
-generated_meanings <- sample(c("aaa", "bbb", "ccc"), row_to_gen, replace = T)
-generated_origins <- sample(c("123", "456", "789"), row_to_gen, replace = T)
+generated_meanings <- sample(as.character(seq_len(n_meanings)), row_to_gen, replace = T)
+generated_origins <- sample(as.character(seq_len(n_origins)), row_to_gen, replace = T)
 
 test_df <- data.table(person_id = generate_ids,
                       op_start_date = generated_dates,
--- examples/02_Speed/to_run.R
+++ examples/02_Speed/to_run.R
@@ -1,6 +1,11 @@
 library(data.table)
 source("R/CreateSpells_v14.R")
 
-row_to_gen <- 1000000
+row_to_gen <- 100000
+mean_n_obs_each_id <- 10
+n_meanings = 3
+n_origins = 3
 
 source("examples/02_Speed/test_speed.R")
+
+
