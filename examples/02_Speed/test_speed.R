943f5ebfcdf0714af326aad9c10fac546637aa74
Davide <davide.messina@ars.toscana.it>
2021-06-08 16:26:12 +0200

    minor changes

--- examples/02_Speed/test_speed.R
+++ examples/02_Speed/test_speed.R
@@ -11,26 +11,36 @@ generated_meanings <- sample(c("aaa", "bbb", "ccc"), row_to_gen, replace = T)
 generated_origins <- sample(c("123", "456", "789"), row_to_gen, replace = T)
 
 test_df <- data.table(person_id = generate_ids,
-                      op_start_date = generated_dates,
+                      op_start_date = format(generated_dates, "%Y%m%d"),
                       op_end_date = generated_dates + generated_integers,
                       op_meaning = generated_meanings,
                       op_origin = generated_origins)
 
 rm(generate_ids, generated_dates, generated_integers, generated_meanings, generated_origins)
 
-test <-  CreateSpells(
+bench::mark(CreateSpells(
   dataset = test_df,
   id = "person_id" ,
   start_date = "op_start_date",
   end_date = "op_end_date",
   category ="op_meaning"
-)
+))
+
+p <- profvis::profvis({
+  CreateSpells(
+    dataset = test_df,
+    id = "person_id" ,
+    start_date = "op_start_date",
+    end_date = "op_end_date",
+    category ="op_meaning"
+  )
+})
 
 fasttime::fastPOSIXct("2010-01-01")
 
-as.Date(generated_dates, "%Y%m%d")
-
+generated_dates <- format(as.Date(generated_dates, "%Y%m%d"), "%Y%m%d")
+generated_dates_1 <- data.table::copy(generated_dates)
 bench::mark(
   lubridate::ymd(generated_dates),
-  as.Date(generated_dates, generated_dates)
+  as.Date(generated_dates_1, "%Y%m%d")
 )
