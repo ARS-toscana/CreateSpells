e983a3d6313ad312bb6c8bdb349e31e0589bc1c6
Davide <davide.messina@ars.toscana.it>
2022-05-21 18:58:27 +0200

    additional vetr criteria, RcppAlgos -> utils and rewriting overlap

--- R/CreateSpells_v15.R
+++ R/CreateSpells_v15.R
@@ -17,7 +17,7 @@
 #' NOTE: Developed under R  4.0.3
 
 
-CreateSpells <- function(dataset, id, start_date, end_date, category, replace_missing_end_date = NA_integer_,
+CreateSpells <- function(dataset, id, start_date, end_date, category = NULL, replace_missing_end_date = NULL,
                          overlap=F, dataset_overlap = "df_overlap", only_overlaps=F, gap_allowed = 1){
   library(data.table)
   library(vetr)
@@ -28,26 +28,35 @@ CreateSpells <- function(dataset, id, start_date, end_date, category, replace_mi
     return(x)
   }
 
-  token_missing_dates <- vetr::vet_token(!(any(is.na(dataset[[end_date]])) && missing(.)),
+  token_missing_dates <- vetr::vet_token(any(is.na(.)),
                                          "it is set to default value %s, however since there are missing dates a custom value should be set")
-  token_replace <- vetr::vet_token(is.ymd_or_date(.) && token_missing_dates,
+  token_replace <- vetr::vet_token(!(!is.ymd_or_date(.) && token_missing_dates),
                                    "%s shoulde be a date or string/integer interpretable by lubridate::ymd")
   token_col <- vetr::vet_token(. %in% colnames(dataset),
                                paste("%s is not a column in", deparse(substitute(dataset))))
-  token_category <- vetr::vet_token(I(isTRUE(.) && length(unique(dataset[[category]])) >= 2 || isFALSE(.)),
+
+  token_exist_categories <- vetr::vet_token(!(isTRUE(.) && is.null(category)),
+                                            "%s is set to TRUE, however the overlaps can not be computed as has category argument has not been specified")
+
+  token_n_categories <- vetr::vet_token(!(isTRUE(.) && length(unique(dataset[[category]])) < 2),
                                     "%s is set to TRUE, however the overlaps can not be computed as has less than two categories")
   token_overlap <- vetr::vet_token(I(. == "df_overlap" || (isTRUE(overlap) || isTRUE(only_overlaps))),
                                     "it is set to %s, however neither overlap or only_overlaps argument are set to TRUE")
 
   vetr::vetr(
-    dataset = data.frame(),
+    dataset = data.frame()
+  )
+
+  dataset = data.table::as.data.table(dataset)
+
+  vetr::vetr(
     id = character(1L) && token_col,
     start_date = character(1L) && token_col,
     end_date = character(1L) && token_col,
-    category = character(1L) && token_col,
-    replace_missing_end_date = token_replace,
-    overlap = logical(1L) && token_category,
-    only_overlaps = logical(1L) && token_category,
+    category = NULL || character(1L) && token_col,
+    replace_missing_end_date = NULL || token_replace,
+    overlap = logical(1L) && token_exist_categories && token_n_categories,
+    only_overlaps = logical(1L) && token_n_categories,
     dataset_overlap = character(1L) && token_overlap,
     gap_allowed = integer(1L)
   )
@@ -74,55 +83,44 @@ CreateSpells <- function(dataset, id, start_date, end_date, category, replace_mi
       dataset<-dataset[get(start_date) < get(end_date)]
     }
 
-
     #add level overall if category is given as input and has more than 1 category
-    if (!missing(category)){
-      if(length(unique(dataset[[category]]))>1) {
-        tmp<-as.data.table(dataset)
-        tmp[[category]]<-c("_overall")
-        dataset <- rbindlist(list(dataset, tmp))
-        rm(tmp)
-        print("The level overall is added as the is more then one category")
-      }
+    if (!is.null(category) & length(unique(dataset[[category]])) >= 2){
+      dataset <- rbindlist(list(dataset, copy(dataset)[, (category) := "_overall"]))
+      print("The level overall is added as the is more than one category")
     }
 
-    #group by and arrange the dataset
-    if(!missing(category)) {
-      setorderv(dataset, c(id, category, start_date, end_date))
+    if(is.null(category)){
+      order_vec <- c(id, start_date, end_date)
+      grouping_vars <- id
     } else {
-      setorderv(dataset, c(id, start_date, end_date))
+      order_vec <- c(id, category, start_date, end_date)
+      grouping_vars <- c(id, category)
     }
+    #group by and arrange the dataset
+    setorderv(dataset, order_vec)
+
+    #row id by group
+    dataset[, row_id := seq_len(.N), by = grouping_vars]
+
+    #lagged end_date
+    dataset[, lag_end_date := fifelse(row_id > 1, shift(get(..end_date)), get(..end_date))]
+
+    # cumulative max for dates
+    dataset[, lag_end_date := as.integer(lag_end_date)]
+    dataset[, lag_end_date := cummax(lag_end_date), by = grouping_vars]
+    dataset[, lag_end_date := as.Date(lag_end_date, "1970-01-01")]
 
     #compute the number of spell
-    year_1900 <- as.Date(lubridate::ymd(19000101))
-
-    if (missing(category)) {
-      grouping_vars <- id
-      dataset<-dataset[, `:=`(row_id = rowid(get(..id)))]
-    } else {
-      grouping_vars <- c(id, category)
-      dataset<-dataset[, row_id := rowid(get(..id), get(..category))]
-    }
-
-    dataset<-dataset[, `:=`(lag_end_date = fifelse(row_id > 1, shift(get(..end_date)), get(..end_date)))]
-    dataset<-dataset[, `:=`(lag_end_date = as.integer(lag_end_date))]
-    dataset<-dataset[, `:=`(lag_end_date = cummax(lag_end_date)), by = grouping_vars]
-    dataset <- dataset[, `:=`(lag_end_date = as.Date(lag_end_date, "1970-01-01"))]
-    dataset <- dataset[, `:=`(num_spell = fifelse(row_id > 1 & get(..start_date) <= lag_end_date + gap_allowed, 0, 1))]
-    dataset<-dataset[, `:=`(num_spell = cumsum(num_spell)), by = grouping_vars]
+    dataset[, num_spell := fifelse(row_id > 1 & get(..start_date) <= lag_end_date + gap_allowed, 0, 1)]
+    dataset[, num_spell := cumsum(num_spell), by = grouping_vars]
 
     #group by num spell and compute min and max date for each one
-    if(!missing(category)) {
-      # dataset<-dataset[, c(entry_spell_category := min(get(..start_date)),exit_spell_category := max(get(..end_date))), by = c(id, category, "num_spell")]
-      # dataset<-dataset[, .(entry_spell_category = min(get(..start_date)),
-      #      exit_spell_category = max(get(..end_date))), keyby = .(id=get(..id), get(..category), num_spell)]
-      myVector <- c(id,category,"num_spell","entry_spell_category","exit_spell_category")
-      dataset <- unique(dataset[, c("entry_spell_category", "exit_spell_category") := list(min(get(..start_date)), max(get(..end_date))), by = c(id, category, "num_spell")][, ..myVector])
-      #
-    }else{
-      myVector <- c(id,"num_spell","entry_spell_category","exit_spell_category")
-      dataset<-unique(dataset[, .(entry_spell_category = min(get(..start_date)), exit_spell_category = max(get(..end_date))), by = c(id, "num_spell")][, ..myVector])
-    }
+    keep_col <- c(grouping_vars, "num_spell", "entry_spell_category", "exit_spell_category")
+    grouping_vars <- c(grouping_vars, "num_spell")
+
+    dataset <- dataset[, .(entry_spell_category = min(get(..start_date)),
+                           exit_spell_category = max(get(..end_date))), by = grouping_vars]
+    dataset <- dataset[, ..keep_col]
 
     assign("output_spells_category", dataset)
   }
@@ -130,43 +128,39 @@ CreateSpells <- function(dataset, id, start_date, end_date, category, replace_mi
   #OPTIONAL SECTION REGARDING OVERLAPS
 
   if(overlap == T || only_overlaps == T){
-    export_df <-data.table()
     dataset <- dataset[get(category) != "_overall",]
 
-    #Create the list of pairs of categories
-    permut <- RcppAlgos::comboGeneral(unique(dataset[[category]]), m = 2)
+    # Create list of unique not missing categories
+    unique_cat <- as.list(unique(dataset[[category]]))
+    unique_cat <- unique_cat[!is.na(unique_cat)]
 
-    #	For each pair of values A and B, create two temporary datasets
-    #vec<-c(id)
-    #dataset<-dataset[, .SD[length(unique(get(..category))) > 1], keyby = vec]
+    # Create the combinations of pairs of categories
+    permut <- t(utils::combn(unique_cat, 2))
 
+    # Cycle for each pair
     for (i in seq_len(nrow(permut))) {
 
       p_1 <- permut[i, 1]
       p_2 <- permut[i, 2]
-
-      if (is.na(p_1) | is.na(p_2)){
-        next
-      }
 
       ens_1 <- paste0("entry_spell_category_", p_1)
       exs_1 <- paste0("exit_spell_category_", p_1)
       ens_2 <- paste0("entry_spell_category_", p_2)
       exs_2 <- paste0("exit_spell_category_", p_2)
 
+      #	For each pair of values A and B, create two temporary datasets
       outputA <- dataset[get(category) == p_1, ][, c("num_spell", category) := NULL]
       setnames(outputA, c("entry_spell_category", "exit_spell_category"), c(ens_1, exs_1))
 
       outputB <- dataset[get(category) == p_2, ][, c("num_spell", category) := NULL]
       setnames(outputB, c("entry_spell_category", "exit_spell_category"), c(ens_2, exs_2))
+
       #	Perform a join multi-to-multi of the two datasets
+      CAT <- merge(outputA, outputB, by = c(id), all = T, allow.cartesian = T)
+      CAT <- CAT[(get(ens_1) <= get(exs_2) + gap_allowed & get(exs_1) + gap_allowed >= get(ens_2)) | (get(ens_2) <= get(exs_1) + gap_allowed & get(exs_2) + gap_allowed >= get(ens_1)), ]
 
-      CAT <- merge(outputA, outputB, by = c(id), all = T)
-      CAT <- CAT[(get(ens_1) <= get(exs_2) + gap_allowed & get(exs_1) + gap_allowed >= get(ens_2)) | (get(ens_2) < get(exs_1) + gap_allowed & get(exs_2) + gap_allowed >= get(ens_1)), ]
-
-      if (dim(CAT)[1] == 0) {
-        next
-      }
+      #	If no overlap go to next pair
+      if (nrow(CAT) == 0) next
 
       CAT <- CAT[, `:=`(entry_spell_category = max(get(ens_1), get(ens_2)),
                         exit_spell_category = min(get(exs_1), get(exs_2))), by = id]
