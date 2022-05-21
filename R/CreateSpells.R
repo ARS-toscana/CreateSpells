55e93e1bca68c5cbdb30e6c01cb6d90ffe03c707
Davide <davide.messina@ars.toscana.it>
2022-05-21 20:03:16 +0200

    deleted old version and renamed current

--- R/CreateSpells.R
+++ R/CreateSpells.R
@@ -22,49 +22,9 @@ CreateSpells <- function(dataset, id, start_date, end_date, category = NULL, rep
   library(data.table)
   library(vetr)
 
-  is.ymd_or_date <- function(x) {
-    x <- tryCatch(lubridate::ymd(x), error=function(e) F, warning=function(w) F)
-    if (lubridate::is.Date(x)) x <- T
-    return(x)
-  }
-
-  token_missing_dates <- vetr::vet_token(any(is.na(.)),
-                                         "it is set to default value %s, however since there are missing dates a custom value should be set")
-  token_replace <- vetr::vet_token(!(!is.ymd_or_date(.) && token_missing_dates),
-                                   "%s shoulde be a date or string/integer interpretable by lubridate::ymd")
-  token_col <- vetr::vet_token(. %in% colnames(dataset),
-                               paste("%s is not a column in", deparse(substitute(dataset))))
-
-  token_exist_categories <- vetr::vet_token(!(isTRUE(.) && is.null(category)),
-                                            "%s is set to TRUE, however the overlaps can not be computed as has category argument has not been specified")
-
-  token_n_categories <- vetr::vet_token(!(isTRUE(.) && length(unique(dataset[[category]])) < 2),
-                                    "%s is set to TRUE, however the overlaps can not be computed as has less than two categories")
-  token_overlap <- vetr::vet_token(I(. == "df_overlap" || (isTRUE(overlap) || isTRUE(only_overlaps))),
-                                    "it is set to %s, however neither overlap or only_overlaps argument are set to TRUE")
-
-  vetr::vetr(
-    dataset = data.frame()
-  )
-
-  dataset = data.table::as.data.table(dataset)
-
-  vetr::vetr(
-    id = character(1L) && token_col,
-    start_date = character(1L) && token_col,
-    end_date = character(1L) && token_col,
-    category = NULL || character(1L) && token_col,
-    replace_missing_end_date = NULL || token_replace,
-    overlap = logical(1L) && token_exist_categories && token_n_categories,
-    only_overlaps = logical(1L) && token_n_categories,
-    dataset_overlap = character(1L) && token_overlap,
-    gap_allowed = integer(1L)
-  )
-
-  token_impossible_period <- vetr::vet_token(all(.[[start_date]] < .[[end_date]], na.rm = T),
-                                             paste("Inside %s, there observation period/s with", deparse(substitute(start_date)),
-                                                   "less than", deparse(substitute(end_date))))
-  vetr::vet(token_impossible_period, dataset, stop = T)
+  mycall <- match.call()
+  mycall[[1]] <- as.symbol("check_sanitize_inputs") # use inner 1
+  dataset <- eval(mycall)
 
   if (!only_overlaps) {
     dataset[, (start_date) := lubridate::ymd(get(..start_date))]
