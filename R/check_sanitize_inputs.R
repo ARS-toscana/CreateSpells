f2904b32b38e83b3637710e69d52f5a70ee2231e
Davide <davide.messina@ars.toscana.it>
2022-06-11 16:14:05 +0200

    added error number to messages

--- R/check_sanitize_inputs.R
+++ R/check_sanitize_inputs.R
@@ -1,6 +1,8 @@
 check_sanitize_inputs <- function(dataset, id, start_date, end_date, category = NULL, replace_missing_end_date = NULL,
                                   overlap=F, dataset_overlap = "df_overlap", only_overlaps=F, gap_allowed = 1) {
 
+  . <- NULL
+
   # Function to check if x is a date or is an input that ymd() can accept
   is.ymd_or_date <- function(x) {
     x <- tryCatch(lubridate::ymd(x), error=function(e) F, warning=function(w) F)
@@ -10,27 +12,33 @@ check_sanitize_inputs <- function(dataset, id, start_date, end_date, category = 
 
   # Check if there are any missing dates
   token_missing_dates <- vetr::vet_token(any(is.na(.)),
-                                         "it is set to default value %s, however since there are missing dates a custom value should be set")
+                                         "it is set to default value %s,
+                                         however since there are missing dates a custom value should be set
+                                         (Error 01)")
 
   # Check if x is/can be a date
   token_replace <- vetr::vet_token(!(!is.ymd_or_date(.) && token_missing_dates),
-                                   "%s shoulde be a date or string/integer interpretable by lubridate::ymd")
+                                   "%s shoulde be a date or string/integer interpretable by lubridate::ymd
+                                   (Error 02)")
 
   # Check if x is a column of dataset
   token_col <- vetr::vet_token(. %in% colnames(dataset),
-                               paste("%s is not a column in", deparse(substitute(dataset))))
+                               paste("%s is not a column in", deparse(substitute(dataset)), "(Error 03)"))
 
   # Want to calculate overlap but no category specified
   token_exist_categories <- vetr::vet_token(!(isTRUE(.) && is.null(category)),
-                                            "%s is set to TRUE, however the overlaps can not be computed as the category argument has not been specified")
+                                            "%s is set to TRUE, however the overlaps can not be computed as
+                                            the category argument has not been specified (Error 04)")
 
   # Want to calculate overlap but insufficient categories
   token_n_categories <- vetr::vet_token(!(isTRUE(.) && length(unique(dataset[[category]])) < 2),
-                                        "%s is set to TRUE, however the overlaps can not be computed as has less than two categories")
+                                        "%s is set to TRUE, however the overlaps can not be computed as
+                                        has less than two categories (Error 05)")
 
   # Check if any overlap is T or if dataset_overlap has default value
   token_overlap <- vetr::vet_token(I(. == "df_overlap" || (isTRUE(overlap) || isTRUE(only_overlaps))),
-                                   "it is set to %s, however neither overlap or only_overlaps argument are set to TRUE")
+                                   "it is set to %s, however neither overlap or only_overlaps
+                                   argument are set to TRUE (Error 06)")
 
   # Check if dataset is a data.frame then trasform it to data.table
   vetr::vetr(dataset = data.frame())
