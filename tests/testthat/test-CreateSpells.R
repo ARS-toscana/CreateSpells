d31e515cf88f8329f79472d78367f28c6ca5bc5b
Davide Messina <davide.messina@ars.toscana.it>
2022-06-12 18:31:31 +0200

    add more parameter to function in test

--- tests/testthat/test-CreateSpells.R
+++ tests/testthat/test-CreateSpells.R
@@ -15,9 +15,14 @@
 # par_excl_dataset[["dataset"]]
 
 expect_floor_equal <- function(dataset = test_data, id = "id", start_date = "op_start_date",
-                               end_date = "op_end_date", error_include) {
+                               end_date = "op_end_date", category = NULL, replace_missing_end_date = NULL,
+                               overlap=F, dataset_overlap = "df_overlap", only_overlaps=F, gap_allowed = 1, error_include) {
   eval(bquote(expect_error(check_sanitize_inputs(dataset = dataset, id = id, start_date = start_date,
-                                                 end_date = end_date), regexp = error_include)))
+                                                 end_date = end_date, category = category,
+                                                 replace_missing_end_date = replace_missing_end_date,
+                                                 overlap = overlap, dataset_overlap = dataset_overlap,
+                                                 only_overlaps = only_overlaps, gap_allowed = gap_allowed),
+                           regexp = error_include)))
 }
 
 test_that("dataset only accepts dataframes", {
