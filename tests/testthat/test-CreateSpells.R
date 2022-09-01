da99f1ba27476217ff3da1322ca0b081d530caa1
Davide Messina <davide.messina@ars.toscana.it>
2022-09-01 20:33:13 +0200

    created a few tests

--- tests/testthat/test-CreateSpells.R
+++ tests/testthat/test-CreateSpells.R
@@ -9,6 +9,113 @@ test_that("single observation", {
   ))
 })
 
+test_that("partial overlap", {
+  expect_identical(CreateSpells(dataset = row_wise_dt(
+    ~id,~start_date, ~end_date,~comment,
+    "a1", "20210101","20220101","single",
+    "a1", "20210601","20230101","single",
+  ), id = id, start_date = start_date , end_date = end_date),
+  row_wise_dt(
+    ~id,~start_date, ~end_date,~comment,
+    "a1", "20210101","20230101","single",
+  ))
+})
+
+test_that("single day overlap", {
+  expect_identical(CreateSpells(dataset = row_wise_dt(
+    ~id,~start_date, ~end_date,~comment,
+    "a1", "20210101","20220101","single",
+    "a1", "20220101","20230101","single",
+  ), id = id, start_date = start_date , end_date = end_date),
+  row_wise_dt(
+    ~id,~start_date, ~end_date,~comment,
+    "a1", "20210101","20230101","single",
+  ))
+})
+
+test_that("perfect overlap", {
+  expect_identical(CreateSpells(dataset = row_wise_dt(
+    ~id,~start_date, ~end_date,~comment,
+    "a1", "20210101","20220101","single",
+    "a1", "20210101","20220101","single",
+  ), id = id, start_date = start_date , end_date = end_date),
+  row_wise_dt(
+    ~id,~start_date, ~end_date,~comment,
+    "a1", "20210101","20220101","single",
+  ))
+})
+
+test_that("inner overlap", {
+  expect_identical(CreateSpells(dataset = row_wise_dt(
+    ~id,~start_date, ~end_date,~comment,
+    "a1", "20210101","20230101","single",
+    "a1", "20210601","20220601","single",
+  ), id = id, start_date = start_date , end_date = end_date),
+  row_wise_dt(
+    ~id,~start_date, ~end_date,~comment,
+    "a1", "20210101","20230101","single",
+  ))
+})
+
+test_that("inner overlap with boundary matching", {
+  expect_identical(CreateSpells(dataset = row_wise_dt(
+    ~id,~start_date, ~end_date,~comment,
+    "a1", "20210101","20230101","single",
+    "a1", "20210101","20220601","single",
+    "a2", "20210101","20230101","single",
+    "a2", "20210601","20230101","single",
+  ), id = id, start_date = start_date , end_date = end_date),
+  row_wise_dt(
+    ~id,~start_date, ~end_date,~comment,
+    "a1", "20210101","20230101","single",
+    "a2", "20210101","20230101","single",
+  ))
+})
+
+test_that("disjoint, gap default", {
+  expect_identical(CreateSpells(dataset = row_wise_dt(
+    ~id,~start_date, ~end_date,~comment,
+    "a1", "20210101","20211231","single",
+    "a1", "20220101","20230101","single",
+    "a2", "20210101","20211231","single",
+    "a2", "20220102","20230101","single",
+  ), id = id, start_date = start_date , end_date = end_date),
+  row_wise_dt(
+    ~id,~start_date, ~end_date,~comment,
+    "a1", "20210101","20230101","single",
+    "a2", "20210101","20211231","single",
+    "a2", "20220102","20230101","single",
+  ))
+})
+
+test_that("triple overlap", {
+  expect_identical(CreateSpells(dataset = row_wise_dt(
+    ~id,~start_date, ~end_date,~comment,
+    "a1", "20210101","20211231","single",
+    "a1", "20210601","20220601","single",
+    "a1", "20220101","20230101","single",
+  ), id = id, start_date = start_date , end_date = end_date),
+  row_wise_dt(
+    ~id,~start_date, ~end_date,~comment,
+    "a1", "20210101","20230101","single",
+  ))
+})
+
+test_that("inner overlap + simple + disjoint", {
+  expect_identical(CreateSpells(dataset = row_wise_dt(
+    ~id,~start_date, ~end_date,~comment,
+    "a1", "20210101","20220601","single",
+    "a1", "20210601","20213112","single",
+    "a1", "20220401","20220801","single",
+    "a1", "20223112","20230101","single",
+  ), id = id, start_date = start_date , end_date = end_date),
+  row_wise_dt(
+    ~id,~start_date, ~end_date,~comment,
+    "a1", "20210101","20220801","single",
+    "a1", "20223112","20230101","single",
+  ))
+})
+
 # row_wise_dt(
 #   ~id,~start_date, ~end_date,~op_meaning,                  ~comment,
 #   "a1", "20100101","20200101",        "a",                  "single",
