b8c8ac4868ce425eae333f326c1b519aa3618aab
Davide Messina <davide.messina@ars.toscana.it>
2023-04-07 20:19:59 +0200

    Added test for partial overlap and fixed previous test

--- tests/testthat/test-CreateSpells.R
+++ tests/testthat/test-CreateSpells.R
@@ -1,123 +1,130 @@
 test_that("single observation no meaning", {
-  expect_identical(CreateSpells(dataset = test_data[to_use_comment == "single"][, op_meaning := NULL],
-                                id = "id", start_date = "op_start_date" , end_date = "op_end_date"),
-                   row_wise_dt(~id ,~num_spell,~entry_spell_category,~exit_spell_category,
-                               "a1",         1,           "20100101",          "20200101"))
+  result <- row_wise_dt(~id ,~num_spell,~entry_spell_category,~exit_spell_category,
+                        "a1",         1,           "20100101",          "20200101")
+  expect_identical(test_CreateSpells.internal(dataset = test_data_1[to_use_comment == "single"][, meaning := NULL]),
+                   result)
+  expect_identical(test_CreateSpells.internal(dataset = test_data_1[to_use_comment == "single"]),
+                   result)
 })
 
-test_that("single observation no meaning", {
-  expect_identical(CreateSpells(dataset = test_data[to_use_comment == "single"],
-                                id = "id", start_date = "op_start_date" , end_date = "op_end_date"),
-                   row_wise_dt(~id ,~num_spell,~entry_spell_category,~exit_spell_category,
-                               "a1",         1,           "20100101",          "20200101"))
+test_that("pass meaning", {
+  expect_identical(test_CreateSpells.internal(dataset = test_data_1[to_use_comment == "single"], category = "meaning"),
+                   row_wise_dt(~id ,~meaning,~num_spell,~entry_spell_category,~exit_spell_category,
+                               "a1",     "a",         1,           "20100101",          "20200101"))
 })
 
 test_that("partial overlap", {
-  expect_identical(CreateSpells(dataset = row_wise_dt(
-    ~id,~start_date, ~end_date,~comment,
-    "a1", "20210101","20220101","single",
-    "a1", "20210601","20230101","single",
-  ), id = id, start_date = start_date , end_date = end_date),
-  row_wise_dt(
-    ~id,~start_date, ~end_date,~comment,
-    "a1", "20210101","20230101","single",
-  ))
+  expect_identical(test_CreateSpells.internal(dataset = test_data_1[to_use_comment == "partial overlap"]),
+                   row_wise_dt(~id ,~num_spell,~entry_spell_category,~exit_spell_category,
+                               "a2",         1,           "20100101",          "20250101"))
 })
 
-test_that("single day overlap", {
-  expect_identical(CreateSpells(dataset = row_wise_dt(
-    ~id,~start_date, ~end_date,~comment,
-    "a1", "20210101","20220101","single",
-    "a1", "20220101","20230101","single",
-  ), id = id, start_date = start_date , end_date = end_date),
-  row_wise_dt(
-    ~id,~start_date, ~end_date,~comment,
-    "a1", "20210101","20230101","single",
-  ))
-})
-
-test_that("perfect overlap", {
-  expect_identical(CreateSpells(dataset = row_wise_dt(
-    ~id,~start_date, ~end_date,~comment,
-    "a1", "20210101","20220101","single",
-    "a1", "20210101","20220101","single",
-  ), id = id, start_date = start_date , end_date = end_date),
-  row_wise_dt(
-    ~id,~start_date, ~end_date,~comment,
-    "a1", "20210101","20220101","single",
-  ))
-})
-
-test_that("inner overlap", {
-  expect_identical(CreateSpells(dataset = row_wise_dt(
-    ~id,~start_date, ~end_date,~comment,
-    "a1", "20210101","20230101","single",
-    "a1", "20210601","20220601","single",
-  ), id = id, start_date = start_date , end_date = end_date),
-  row_wise_dt(
-    ~id,~start_date, ~end_date,~comment,
-    "a1", "20210101","20230101","single",
-  ))
-})
-
-test_that("inner overlap with boundary matching", {
-  expect_identical(CreateSpells(dataset = row_wise_dt(
-    ~id,~start_date, ~end_date,~comment,
-    "a1", "20210101","20230101","single",
-    "a1", "20210101","20220601","single",
-    "a2", "20210101","20230101","single",
-    "a2", "20210601","20230101","single",
-  ), id = id, start_date = start_date , end_date = end_date),
-  row_wise_dt(
-    ~id,~start_date, ~end_date,~comment,
-    "a1", "20210101","20230101","single",
-    "a2", "20210101","20230101","single",
-  ))
-})
-
-test_that("disjoint, gap default", {
-  expect_identical(CreateSpells(dataset = row_wise_dt(
-    ~id,~start_date, ~end_date,~comment,
-    "a1", "20210101","20211231","single",
-    "a1", "20220101","20230101","single",
-    "a2", "20210101","20211231","single",
-    "a2", "20220102","20230101","single",
-  ), id = id, start_date = start_date , end_date = end_date),
-  row_wise_dt(
-    ~id,~start_date, ~end_date,~comment,
-    "a1", "20210101","20230101","single",
-    "a2", "20210101","20211231","single",
-    "a2", "20220102","20230101","single",
-  ))
-})
-
-test_that("triple overlap", {
-  expect_identical(CreateSpells(dataset = row_wise_dt(
-    ~id,~start_date, ~end_date,~comment,
-    "a1", "20210101","20211231","single",
-    "a1", "20210601","20220601","single",
-    "a1", "20220101","20230101","single",
-  ), id = id, start_date = start_date , end_date = end_date),
-  row_wise_dt(
-    ~id,~start_date, ~end_date,~comment,
-    "a1", "20210101","20230101","single",
-  ))
-})
-
-test_that("inner overlap + simple + disjoint", {
-  expect_identical(CreateSpells(dataset = row_wise_dt(
-    ~id,~start_date, ~end_date,~comment,
-    "a1", "20210101","20220601","single",
-    "a1", "20210601","20213112","single",
-    "a1", "20220401","20220801","single",
-    "a1", "20223112","20230101","single",
-  ), id = id, start_date = start_date , end_date = end_date),
-  row_wise_dt(
-    ~id,~start_date, ~end_date,~comment,
-    "a1", "20210101","20220801","single",
-    "a1", "20223112","20230101","single",
-  ))
-})
+# test_that("partial overlap", {
+#   expect_identical(CreateSpells(dataset = row_wise_dt(
+#     ~id,~start_date, ~end_date,~comment,
+#     "a1", "20210101","20220101","single",
+#     "a1", "20210601","20230101","single",
+#   ), id = id, start_date = start_date , end_date = end_date),
+#   row_wise_dt(
+#     ~id,~start_date, ~end_date,~comment,
+#     "a1", "20210101","20230101","single",
+#   ))
+# })
+#
+# test_that("single day overlap", {
+#   expect_identical(CreateSpells(dataset = row_wise_dt(
+#     ~id,~start_date, ~end_date,~comment,
+#     "a1", "20210101","20220101","single",
+#     "a1", "20220101","20230101","single",
+#   ), id = id, start_date = start_date , end_date = end_date),
+#   row_wise_dt(
+#     ~id,~start_date, ~end_date,~comment,
+#     "a1", "20210101","20230101","single",
+#   ))
+# })
+#
+# test_that("perfect overlap", {
+#   expect_identical(CreateSpells(dataset = row_wise_dt(
+#     ~id,~start_date, ~end_date,~comment,
+#     "a1", "20210101","20220101","single",
+#     "a1", "20210101","20220101","single",
+#   ), id = id, start_date = start_date , end_date = end_date),
+#   row_wise_dt(
+#     ~id,~start_date, ~end_date,~comment,
+#     "a1", "20210101","20220101","single",
+#   ))
+# })
+#
+# test_that("inner overlap", {
+#   expect_identical(CreateSpells(dataset = row_wise_dt(
+#     ~id,~start_date, ~end_date,~comment,
+#     "a1", "20210101","20230101","single",
+#     "a1", "20210601","20220601","single",
+#   ), id = id, start_date = start_date , end_date = end_date),
+#   row_wise_dt(
+#     ~id,~start_date, ~end_date,~comment,
+#     "a1", "20210101","20230101","single",
+#   ))
+# })
+#
+# test_that("inner overlap with boundary matching", {
+#   expect_identical(CreateSpells(dataset = row_wise_dt(
+#     ~id,~start_date, ~end_date,~comment,
+#     "a1", "20210101","20230101","single",
+#     "a1", "20210101","20220601","single",
+#     "a2", "20210101","20230101","single",
+#     "a2", "20210601","20230101","single",
+#   ), id = id, start_date = start_date , end_date = end_date),
+#   row_wise_dt(
+#     ~id,~start_date, ~end_date,~comment,
+#     "a1", "20210101","20230101","single",
+#     "a2", "20210101","20230101","single",
+#   ))
+# })
+#
+# test_that("disjoint, gap default", {
+#   expect_identical(CreateSpells(dataset = row_wise_dt(
+#     ~id,~start_date, ~end_date,~comment,
+#     "a1", "20210101","20211231","single",
+#     "a1", "20220101","20230101","single",
+#     "a2", "20210101","20211231","single",
+#     "a2", "20220102","20230101","single",
+#   ), id = id, start_date = start_date , end_date = end_date),
+#   row_wise_dt(
+#     ~id,~start_date, ~end_date,~comment,
+#     "a1", "20210101","20230101","single",
+#     "a2", "20210101","20211231","single",
+#     "a2", "20220102","20230101","single",
+#   ))
+# })
+#
+# test_that("triple overlap", {
+#   expect_identical(CreateSpells(dataset = row_wise_dt(
+#     ~id,~start_date, ~end_date,~comment,
+#     "a1", "20210101","20211231","single",
+#     "a1", "20210601","20220601","single",
+#     "a1", "20220101","20230101","single",
+#   ), id = id, start_date = start_date , end_date = end_date),
+#   row_wise_dt(
+#     ~id,~start_date, ~end_date,~comment,
+#     "a1", "20210101","20230101","single",
+#   ))
+# })
+#
+# test_that("inner overlap + simple + disjoint", {
+#   expect_identical(CreateSpells(dataset = row_wise_dt(
+#     ~id,~start_date, ~end_date,~comment,
+#     "a1", "20210101","20220601","single",
+#     "a1", "20210601","20213112","single",
+#     "a1", "20220401","20220801","single",
+#     "a1", "20223112","20230101","single",
+#   ), id = id, start_date = start_date , end_date = end_date),
+#   row_wise_dt(
+#     ~id,~start_date, ~end_date,~comment,
+#     "a1", "20210101","20220801","single",
+#     "a1", "20223112","20230101","single",
+#   ))
+# })
 
 # row_wise_dt(
 #   ~id,~start_date, ~end_date,~op_meaning,                  ~comment,
