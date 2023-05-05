3cb0db1e32ca97981cceaefccfb83f60919f4e69
Davide <davide.messina@ars.toscana.it>
2023-05-05 17:40:54 +0200

    better test data and helper functions

--- tests/testthat/helper-data-creation.R
+++ tests/testthat/helper-data-creation.R
@@ -27,45 +27,7 @@ create_test_df <- function() {
     "a1",    "20100101",  "20200101",        "c",        "exist in a and b",
     "b1",    "20150101",  "20200101",        "c",         "exist only in b"
   )
-  return(data.table::as.data.table(temp_df))
+  return(temp_df)
 }
 
 test_data <- create_test_df()
-
-create_test_df_1 <- function() {
-  temp_df <- tibble::tribble(
-    ~id, ~st,~en,~meaning,           ~to_use_comment,
-    #---|---|---|--------|--------------------------|
-    "a1", 10, 20,      "a",                  "single",
-    "a2", 10, 20,      "a",         "partial overlap",
-    "a2", 15, 25,      "a",         "partial overlap",
-    "aa", 10, 20,      "a",               "identical",
-    "aa", 10, 20,      "a",               "identical",
-    "a3", 10, 20,      "a",                  "subset",
-    "a3", 15, 16,      "a",                  "subset",
-    "a4", 10, 15,      "a",                "disjoint",
-    "a4", 18, 20,      "a",                "disjoint",
-    "a5", 10, 20,      "a",                   "mixed",
-    "a5", 11, 13,      "a",                   "mixed",
-    "a5", 14, 16,      "a",                   "mixed",
-    "a5", 15, 18,      "a",                   "mixed",
-    "a5", 17, 21,      "a",                   "mixed",
-    "a5", 22, NA,      "a",                   "mixed",
-    "a6", 10, 20,      "a",                 "mixed 2",
-    "a6", 11, 13,      "a",                 "mixed 2",
-    "a6", 14, 21,      "a",                 "mixed 2",
-    "b1", 10, NA,      "b",          "not exist in a",
-    "a1", 15, NA,      "b",  "exist, partial overlap",
-    "a2", 12, 20,      "b", "exist, complete overlap",
-    "a3", 21, NA,      "b",       "exist, no overlap",
-    "a4", 16, 17,      "b", "exist, between disjoint",
-    "c1", 10, 20,      "c",     "not exist in a or b",
-    "a1", 10, 20,      "c",        "exist in a and b",
-    "b1", 15, 20,      "c",         "exist only in b"
-  )
-  return(data.table::as.data.table(temp_df))
-}
-
-test_data_1 <- create_test_df_1()
-test_data_1 <- test_data_1[!(is.na(st)), st := as.integer(paste0("20", st, "0101"))][, st := lubridate::ymd(st)]
-test_data_1 <- test_data_1[!(is.na(en)), en := as.integer(paste0("20", en, "0101"))][, en := lubridate::ymd(en)]
--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,6 +1,7 @@
 test_error_type <- function(dataset = test_data, id = "id", start_date = "op_start_date",
                             end_date = "op_end_date", category = NULL, replace_missing_end_date = NULL,
-                            overlap=F, dataset_overlap = NA_character_, only_overlaps=F, gap_allowed = 1, error_include) {
+                            overlap = F, dataset_overlap = NA_character_, only_overlaps = F, gap_allowed = 1,
+                            error_include) {
   eval(bquote(expect_error(check_sanitize_inputs(dataset = dataset, id = id, start_date = start_date,
                                                  end_date = end_date, category = category,
                                                  replace_missing_end_date = replace_missing_end_date,
@@ -9,34 +10,67 @@ test_error_type <- function(dataset = test_data, id = "id", start_date = "op_sta
                            regexp = error_include)))
 }
 
-test_CreateSpells.internal <- function(..., id = "id", start_date = "start_date",
-                                       end_date = "end_date", category = NULL, gap_allowed = 1) {
+expect_no_error_with_defaults <- function(dataset = test_data, id = "id", start_date = "op_start_date",
+                                          end_date = "op_end_date", category = NULL, replace_missing_end_date = NULL,
+                                          overlap = F, dataset_overlap = NA_character_, only_overlaps = F,
+                                          gap_allowed = 1) {
+  expect_no_error(check_sanitize_inputs(dataset = dataset, id = id, start_date = start_date,
+                                        end_date = end_date, category = category,
+                                        replace_missing_end_date = replace_missing_end_date,
+                                        overlap = overlap, dataset_overlap = dataset_overlap,
+                                        only_overlaps = only_overlaps, gap_allowed = gap_allowed))
+}
+
+test_CreateSpells.internal <- function(..., id = "id", start_date = "start_date", end_date = "end_date",
+                                       category = NULL, gap_allowed = 1) {
   dataset <- row_wise_dt(...)
   CreateSpells.internal(dataset = dataset, id = id, start_date = start_date, end_date = end_date,
                         category = category, gap_allowed = gap_allowed)
 }
 
-test_type_mult <- function(col_name, error_include, not_allowed_values, ...) {
-  for (i in not_allowed_values) {
-    arguments <- list(...)
-    simple_list <- list(i, error_include)
-    simple_list <- c(arguments, simple_list)
-    names(simple_list) <- c(col_name, "error_include")
-    do.call(test_error_type, simple_list)
-  }
+test_overlap.internal <- function(..., id = "id", start_date = "start_date", end_date = "end_date",
+                                  category = "category", gap_allowed = 1) {
+  dataset <- row_wise_dt(...)
+  overlap.internal(dataset = dataset, id = id, start_date = start_date, end_date = end_date,
+                   category = category, gap_allowed = gap_allowed)
 }
+
+test_data_preparation <- function(..., start_date = "start_date", end_date = "end_date",
+                                  replace_missing_end_date = NULL) {
+  dataset <- data.table::as.data.table(tibble::tribble(...))
+  data_preparation(dataset = dataset, start_date = start_date, end_date = end_date,
+                   replace_missing_end_date = replace_missing_end_date)
+}
+
+# test_type_mult <- function(col_name, error_include, not_allowed_values, ...) {
+#   for (i in not_allowed_values) {
+#     arguments <- list(...)
+#     simple_list <- list(i, error_include)
+#     simple_list <- c(arguments, simple_list)
+#     names(simple_list) <- c(col_name, "error_include")
+#     do.call(test_error_type, simple_list)
+#   }
+# }
 
 row_wise_dt <- function(...) {
   tmp <- data.table::as.data.table(tibble::tribble(...))
   if ("entry_spell_category" %in% colnames(tmp)) {
     tmp[, entry_spell_category := lubridate::ymd(entry_spell_category)]
-  } else {
+  } else if ("start_date" %in% colnames(tmp)) {
     tmp[, start_date := lubridate::ymd(start_date)]
+  } else {
+    tmp[, op_start_date := lubridate::ymd(op_start_date)]
   }
   if ("exit_spell_category" %in% colnames(tmp)) {
     tmp[, exit_spell_category := lubridate::ymd(exit_spell_category)]
-  } else {
+  } else if ("end_date" %in% colnames(tmp)) {
     tmp[, end_date := lubridate::ymd(end_date)]
+  } else {
+    tmp[, op_end_date := lubridate::ymd(op_end_date)]
   }
+  if ("num_spell" %in% colnames(tmp)) {
+    tmp[, num_spell := as.integer(num_spell)]
+  }
+  return(tmp)
 }
 
