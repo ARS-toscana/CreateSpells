320eab7ebb4ba89ccd01cffc02666b08aeeb755d
Davide <davide.messina@ars.toscana.it>
2023-05-05 17:41:14 +0200

    Finish test create spells

--- tests/testthat/test-CreateSpells.R
+++ tests/testthat/test-CreateSpells.R
@@ -109,39 +109,81 @@ test_that("check gap_allowed", {
                                "a",         2,           "20150103",          "20200101"))
   expect_identical(test_CreateSpells.internal(~id, ~start_date,  ~end_date,
                                               "a",  "20100101", "20150101",
-                                              "a",  "20150103", "20200101", gap_allowed = 2),
+                                              "a",  "20150102", "20200101", gap_allowed = 2),
+                   row_wise_dt(~id,~num_spell,~entry_spell_category,~exit_spell_category,
+                               "a",         1,           "20100101",          "20200101"))
+  expect_identical(test_CreateSpells.internal(~id, ~start_date,  ~end_date,
+                                              "a",  "20100101", "20150101",
+                                              "a",  "20150102", "20200101", gap_allowed = 0),
+                   row_wise_dt(~id,~num_spell,~entry_spell_category,~exit_spell_category,
+                               "a",         1,           "20100101",          "20150101",
+                               "a",         2,           "20150102",          "20200101"))
+
+  expect_identical(test_CreateSpells.internal(~id, ~start_date,  ~end_date,
+                                              "a",  "20100101", "20150101",
+                                              "a",  "20150101", "20200101", gap_allowed = -1),
+                   row_wise_dt(~id,~num_spell,~entry_spell_category,~exit_spell_category,
+                               "a",         1,           "20100101",          "20150101",
+                               "a",         2,           "20150101",          "20200101"))
+
+})
+
+test_that("triple simple overlap", {
+  expect_identical(test_CreateSpells.internal(~id, ~start_date,  ~end_date,
+                                              "a",  "20100101", "20120101",
+                                              "a",  "20110101", "20190101",
+                                              "a",  "20180101", "20200101"),
                    row_wise_dt(~id,~num_spell,~entry_spell_category,~exit_spell_category,
                                "a",         1,           "20100101",          "20200101"))
 })
 
+test_that("simple overlap + subset", {
+  expect_identical(test_CreateSpells.internal(~id, ~start_date,  ~end_date,
+                                              "a",  "20100101", "20150101",
+                                              "a",  "20120101", "20190101",
+                                              "a",  "20140101", "20200101",
+                                              "a",  "20180101", "20200101"),
+                   row_wise_dt(~id,~num_spell,~entry_spell_category,~exit_spell_category,
+                               "a",         1,           "20100101",          "20200101"))
+})
 
+test_that("id independence", {
+  expect_identical(test_CreateSpells.internal(~id, ~start_date,  ~end_date,
+                                              "a",  "20100101", "20200101",
+                                              "b",  "20100101", "20200101"),
+                   row_wise_dt(~id,~num_spell,~entry_spell_category,~exit_spell_category,
+                               "a",         1,           "20100101",          "20200101",
+                               "b",         1,           "20100101",          "20200101"))
+})
 
+test_that("id and meaning independence", {
+  expect_identical(test_CreateSpells.internal(~id, ~start_date,  ~end_date, ~meaning,
+                                              "a",  "20100101", "20200101",      "a",
+                                              "b",  "20100101", "20200101",      "a",
+                                              "a",  "20100101", "20200101",      "b",
+                                              "b",  "20100101", "20200101",      "b",
+                                              category = "meaning"),
+                   row_wise_dt(~id,~meaning,~num_spell,~entry_spell_category,~exit_spell_category,
+                               "a",     "a",         1,           "20100101",          "20200101",
+                               "a",     "b",         1,           "20100101",          "20200101",
+                               "b",     "a",         1,           "20100101",          "20200101",
+                               "b",     "b",         1,           "20100101",          "20200101"))
+})
 
-# test_that("triple overlap", {
-#   expect_identical(CreateSpells(dataset = row_wise_dt(
-#     ~id,~start_date, ~end_date,~comment,
-#     "a1", "20210101","20211231","single",
-#     "a1", "20210601","20220601","single",
-#     "a1", "20220101","20230101","single",
-#   ), id = id, start_date = start_date , end_date = end_date),
-#   row_wise_dt(
-#     ~id,~start_date, ~end_date,~comment,
-#     "a1", "20210101","20230101","single",
-#   ))
-# })
-#
-# test_that("inner overlap + simple + disjoint", {
-#   expect_identical(CreateSpells(dataset = row_wise_dt(
-#     ~id,~start_date, ~end_date,~comment,
-#     "a1", "20210101","20220601","single",
-#     "a1", "20210601","20213112","single",
-#     "a1", "20220401","20220801","single",
-#     "a1", "20223112","20230101","single",
-#   ), id = id, start_date = start_date , end_date = end_date),
-#   row_wise_dt(
-#     ~id,~start_date, ~end_date,~comment,
-#     "a1", "20210101","20220801","single",
-#     "a1", "20223112","20230101","single",
-#   ))
-# })
+test_that("reorder periods", {
+  expect_identical(test_CreateSpells.internal(~id, ~start_date,  ~end_date,
+                                              "a",  "20190101", "20200101",
+                                              "a",  "20100101", "20110101"),
+                   row_wise_dt(~id,~num_spell,~entry_spell_category,~exit_spell_category,
+                               "a",         1,           "20100101",          "20110101",
+                               "a",         2,           "20190101",          "20200101",))
+})
 
+test_that("Arguments with same names or different ones", {
+
+  expect_identical(test_CreateSpells.internal(~id, ~start_date,  ~end_date, ~category,
+                                              "id", "20100101", "20200101","category",
+                                              category = "category"),
+                   row_wise_dt(~id,  ~category,~num_spell,~entry_spell_category,~exit_spell_category,
+                               "id","category",         1,           "20100101",          "20200101"))
+})
