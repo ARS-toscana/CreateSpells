6d8a7a8524b9ac64914221beceaf95648dcbadc4
Davide <davide.messina@ars.toscana.it>
2023-05-23 11:38:56 +0200

    Cleaned row_wise_dt and renamed test_data datasets

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -9,7 +9,7 @@ test_error_type <- function(dataset = test_data, id = "person_id", start_date = 
                regexp = error_include)
 }
 
-test_error_type_3 <- function(dataset = test_data_3, id = "id", start_date = "start_date",
+test_error_type_3 <- function(dataset = test_data_special_names, id = "id", start_date = "start_date",
                               end_date = "end_date", category = NULL, replace_missing_end_date = NULL,
                               overlap = F, dataset_overlap = NA_character_, only_overlaps = F, gap_allowed = 1,
                               error_include) {
@@ -20,14 +20,14 @@ test_error_type_3 <- function(dataset = test_data_3, id = "id", start_date = "st
                regexp = error_include)
 }
 
-test_error_type_2 <- function(dataset = test_data_2, id = "person_id", start_date = "op_start_date",
+test_error_type_2 <- function(dataset = test_data_overlap, id = "person_id", start_date = "op_start_date",
                               end_date = "op_end_date", category = "op_meaning", gap_allowed = 1, error_include) {
   expect_error(sanitize_inputs_overlap(dataset = dataset, id = id, start_date = start_date, end_date = end_date,
                                        category = category, gap_allowed = gap_allowed),
                regexp = error_include)
 }
 
-test_error_type_4 <- function(dataset = test_data_4, id = "id", start_date = "start_date", end_date = "end_date",
+test_error_type_4 <- function(dataset = test_data_overlap_special_names, id = "id", start_date = "start_date", end_date = "end_date",
                               category = "category", gap_allowed = 1, error_include) {
   expect_error(sanitize_inputs_overlap(dataset = dataset, id = id, start_date = start_date, end_date = end_date,
                                        category = category, gap_allowed = gap_allowed),
@@ -44,7 +44,7 @@ expect_no_error_with_defaults <- function(dataset = test_data, id = "person_id",
                                   gap_allowed = gap_allowed))
 }
 
-expect_no_error_with_defaults_3 <- function(dataset = test_data_3, id = "id", start_date = "start_date",
+expect_no_error_with_defaults_3 <- function(dataset = test_data_special_names, id = "id", start_date = "start_date",
                                             end_date = "end_date", category = NULL, replace_missing_end_date = NULL,
                                             overlap = F, dataset_overlap = NA_character_, only_overlaps = F,
                                             gap_allowed = 1) {
@@ -54,13 +54,13 @@ expect_no_error_with_defaults_3 <- function(dataset = test_data_3, id = "id", st
                                   gap_allowed = gap_allowed))
 }
 
-expect_no_error_with_defaults_2 <- function(dataset = test_data_2, id = "person_id", start_date = "op_start_date",
+expect_no_error_with_defaults_2 <- function(dataset = test_data_overlap, id = "person_id", start_date = "op_start_date",
                                             end_date = "op_end_date", category = "op_meaning", gap_allowed = 1) {
   expect_no_error(sanitize_inputs_overlap(dataset = dataset, id = id, start_date = start_date, end_date = end_date,
                                           category = category, gap_allowed = gap_allowed))
 }
 
-expect_no_error_with_defaults_4 <- function(dataset = test_data_4, id = "id", start_date = "start_date",
+expect_no_error_with_defaults_4 <- function(dataset = test_data_overlap_special_names, id = "id", start_date = "start_date",
                                             end_date = "end_date", category = "category", gap_allowed = 1) {
   expect_no_error(sanitize_inputs_overlap(dataset = dataset, id = id, start_date = start_date, end_date = end_date,
                                           category = category, gap_allowed = gap_allowed))
@@ -100,34 +100,20 @@ test_data_preparation_2 <- function(..., start_date = "start_date", end_date = "
                    replace_missing_end_date = replace_missing_end_date)
 }
 
-# test_type_mult <- function(col_name, error_include, not_allowed_values, ...) {
-#   for (i in not_allowed_values) {
-#     arguments <- list(...)
-#     simple_list <- list(i, error_include)
-#     simple_list <- c(arguments, simple_list)
-#     names(simple_list) <- c(col_name, "error_include")
-#     do.call(test_error_type, simple_list)
-#   }
-# }
-
 row_wise_dt <- function(...) {
   tmp <- data.table::as.data.table(tibble::tribble(...))
-  if ("entry_spell_category" %in% colnames(tmp)) {
-    tmp[, entry_spell_category := lubridate::ymd(entry_spell_category)]
-  } else if ("start_date" %in% colnames(tmp)) {
-    tmp[, start_date := lubridate::ymd(start_date)]
-  } else {
-    tmp[, op_start_date := lubridate::ymd(op_start_date)]
-  }
-  if ("exit_spell_category" %in% colnames(tmp)) {
-    tmp[, exit_spell_category := lubridate::ymd(exit_spell_category)]
-  } else if ("end_date" %in% colnames(tmp)) {
-    tmp[, end_date := lubridate::ymd(end_date)]
-  } else {
-    tmp[, op_end_date := lubridate::ymd(op_end_date)]
-  }
+
+  start_col <- intersect(colnames(tmp),
+                         c("entry_spell_category", "start_date", "op_start_date"))
+  if (length(start_col) != 0) tmp[, (start_col) := lubridate::ymd(get(..start_col))]
+
+  end_col <- intersect(colnames(tmp),
+                         c("exit_spell_category", "end_date", "op_end_date"))
+  if (length(end_col) != 0) tmp[, (end_col) := lubridate::ymd(get(..end_col))]
+
   if ("num_spell" %in% colnames(tmp)) {
     tmp[, num_spell := as.integer(num_spell)]
   }
+
   return(tmp)
 }
--- tests/testthat/test-parameter_dependencies.R
+++ tests/testthat/test-parameter_dependencies.R
@@ -1,8 +1,8 @@
 test_that("Error 01 if replace_missing_end_date is missing", {
   test_error_type(replace_missing_end_date = NA, error_include = "Error 01")
-  test_error_type_3(dataset = test_data_3, replace_missing_end_date = NA, error_include = "Error 01")
+  test_error_type_3(dataset = test_data_special_names, replace_missing_end_date = NA, error_include = "Error 01")
   expect_no_error_with_defaults(replace_missing_end_date = "20100101")
-  expect_no_error_with_defaults_3(dataset = test_data_3, replace_missing_end_date = "20100101")
+  expect_no_error_with_defaults_3(dataset = test_data_special_names, replace_missing_end_date = "20100101")
 })
 
 test_that("Error 02 if replace_missing_end_date is not a date", {
@@ -61,7 +61,7 @@ test_that("Error 05 if want to calculate overlap with only one category", {
   test_error_type(overlap = T, error_include = "Error 05", category = "op_meaning",
                   dataset = test_data[test_data$op_meaning == "a", ])
   test_error_type_3(overlap = T, error_include = "Error 05", category = "category",
-                  dataset = test_data_3[test_data_3$category == "a", ])
+                  dataset = test_data_special_names[test_data_special_names$category == "a", ])
   expect_no_error_with_defaults(overlap = T, category = "op_meaning")
   expect_no_error_with_defaults_3(overlap = T, category = "category")
 
@@ -69,7 +69,7 @@ test_that("Error 05 if want to calculate overlap with only one category", {
                   dataset = test_data[test_data$op_meaning == "a", ])
 
   test_error_type_3(only_overlaps = T, error_include = "Error 05", category = "category",
-                  dataset = test_data_3[test_data_3$category == "a", ])
+                  dataset = test_data_special_names[test_data_special_names$category == "a", ])
   expect_no_error_with_defaults(only_overlaps = T, category = "op_meaning")
   expect_no_error_with_defaults_3(only_overlaps = T, category = "category")
 })
@@ -176,25 +176,25 @@ test_that("Error 01 if specified column name is not in dataset", {
 })
 
 test_that("Error 02 if some start dates are missing", {
-  test_data_tmp <- test_data_2
+  test_data_tmp <- test_data_overlap
   test_data_tmp[1, "op_start_date"] <- NA
   test_error_type_2(dataset = test_data_tmp, error_include = "Error 02")
 })
 
 test_that("Error 02 if some start dates are missing", {
-  test_data_tmp <- test_data_2
+  test_data_tmp <- test_data_overlap
   test_data_tmp[1, "op_start_date"] <- NA
   test_error_type_2(dataset = test_data_tmp, error_include = "Error 02")
 })
 
 test_that("Error 03 if some end dates are missing", {
-  test_data_tmp <- test_data_2
+  test_data_tmp <- test_data_overlap
   test_data_tmp[1, "op_end_date"] <- NA
   test_error_type_2(dataset = test_data_tmp, error_include = "Error 03")
 })
 
 test_that("Error 04 if some start dates are not interpretable as dates", {
-  test_data_tmp <- test_data_2
+  test_data_tmp <- test_data_overlap
   test_data_tmp$op_start_date <- as.integer(test_data_tmp$op_start_date)
   test_data_tmp[1, "op_start_date"] <- 123456
   test_error_type_2(dataset = test_data_tmp, error_include = "Error 04")
@@ -218,7 +218,7 @@ test_that("Error 04 if some start dates are not interpretable as dates", {
 })
 
 test_that("Error 05 if some end dates are not interpretable as dates", {
-  test_data_tmp <- test_data_2
+  test_data_tmp <- test_data_overlap
   test_data_tmp$op_end_date <- as.integer(test_data_tmp$op_end_date)
   test_data_tmp[1, "op_end_date"] <- 123456
   test_error_type_2(dataset = test_data_tmp, error_include = "Error 05")
@@ -242,14 +242,14 @@ test_that("Error 05 if some end dates are not interpretable as dates", {
 })
 
 test_that("Error 06 if some start dates are after their respective end dates", {
-  test_data_tmp <- test_data_2
+  test_data_tmp <- test_data_overlap
   test_data_tmp[1, "op_start_date"] <- "20200101"
   test_data_tmp[1, "op_end_date"] <- "20100101"
   test_error_type_2(dataset = test_data_tmp, error_include = "Error 06")
 })
 
 test_that("Error 07 if a person has overlapping periods within categories", {
-  test_data_tmp <- test_data_2
+  test_data_tmp <- test_data_overlap
   test_data_tmp[1, "person_id"] <- "a2"
   test_data_tmp[1, "op_start_date"] <- "20110101"
   test_data_tmp[1, "op_end_date"] <- "20210101"
